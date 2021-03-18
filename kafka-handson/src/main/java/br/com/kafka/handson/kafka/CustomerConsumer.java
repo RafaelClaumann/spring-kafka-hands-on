916a039ffef603b44bc42e119a47af2a0894890b
rafael claumann bernardes <rafael_claumann@hotmail.com>
2021-03-18 16:35:50 -0300

    versao simplificada de um retry com nack(sleeptime)

--- kafka-handson/src/main/java/br/com/kafka/handson/kafka/CustomerConsumer.java
+++ kafka-handson/src/main/java/br/com/kafka/handson/kafka/CustomerConsumer.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -16,14 +17,19 @@ public class CustomerConsumer {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
+	// Acknowledgment é injetado pelo spring devido a propriedade: spring.kafka.consumer.enable-auto-commit=false
 	@KafkaHandler
-	public void consume(@Payload Customer customer) {
+	public void consume(@Payload Customer customer, Acknowledgment ack) {
 		LOGGER.info("receiving Customer created, name: {}, id: {}", customer.getNome(), customer.getIdentificacao());
+		ack.acknowledge();  // faz o commit
 	}
 
 	@KafkaHandler
-	public void consume(@Payload VipCustomer vipCustomer) {
+	public void consume(@Payload VipCustomer vipCustomer,Acknowledgment ack) {
 		LOGGER.info("receiving VipCustomer created, name: {}, id: {}, vipId: {}", vipCustomer.getNome(),
 				vipCustomer.getIdentificacao(), vipCustomer.getVipId());
+		
+		// faz um RETRY a cada 5 segundos até conseguir commitar a mensagem, nesse caso vai ficar em loop eterno.
+		ack.nack(5000);
 	}
 }
