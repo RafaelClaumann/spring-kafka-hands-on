1e2794e1bfb1de9153d9d6ae1e8eaa255d993fdf
rafael claumann bernardes <rafael_claumann@hotmail.com>
2021-03-18 14:15:10 -0300

    criando consumer e configs dele no application.properties

---
+++ kafka-handson/src/main/java/br/com/kafka/handson/kafka/CustomerConsumer.java
@@ -0,0 +1,13 @@
+package br.com.kafka.handson.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerConsumer {
+
+	@KafkaListener(topics = "test-topic")
+	public void consume(String mensagem) {
+		System.out.println(mensagem);
+	}
+}
