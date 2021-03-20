c2302200598d48da7f9905aed75dce2e38bba279
rafael claumann bernardes <rafael_claumann@hotmail.com>
2021-03-20 14:13:41 -0300

    update test postLibraryEvent using EmbeddedKafka

--- udemy-course/library-events-producer/src/test/java/integration/com/learnkafka/controllers/LibraryEventsControllerTest.java
+++ udemy-course/library-events-producer/src/test/java/integration/com/learnkafka/controllers/LibraryEventsControllerTest.java
@@ -2,16 +2,28 @@ package com.learnkafka.controllers;
 
 import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvent;
-import org.junit.jupiter.api.Assertions;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @EmbeddedKafka(topics = {"library-events"}, partitions = 3)
@@ -21,33 +33,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class LibraryEventsControllerTest {
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private TestRestTemplate restTemplate;
 
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setup() {
+        Map<String, Object> config = new HashMap<>(KafkaTestUtils.consumerProps("group-1", "true", embeddedKafkaBroker));
+        consumer = new DefaultKafkaConsumerFactory<>(config, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
 
     @Test
-    void postLibraryEvent() {
+    @Timeout(5)
+    void postLibraryEventTest() throws InterruptedException {
         // given
-        Book book = Book.builder()
-                .bookId(456)
-                .bookName("kafka book")
-                .bookAuthor("rafael claumann")
-                .build();
-
-        LibraryEvent libraryEvent = LibraryEvent.builder()
-                .libraryEventId(1)
-                .book(book)
-                .build();
+        Book book = Book.builder().bookId(456).bookName("kafka book").bookAuthor("rafael claumann").build();
+        LibraryEvent libraryEvent = LibraryEvent.builder().libraryEventId(1).book(book).build();
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("content-type", MediaType.APPLICATION_JSON.toString());
-        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent);
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+
+        ConsumerRecord<Integer, String> consumerRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+        String expectedRecord = "{\"libraryEventId\":1,\"book\":{\"bookId\":456,\"bookName\":\"kafka book\",\"bookAuthor\":\"rafael claumann\"},\"eventType\":\"NEW\"}";
+        String responseBody = consumerRecord.value();
 
         // when
-        ResponseEntity<LibraryEvent> response = testRestTemplate.exchange("/v1/library-event", HttpMethod.POST, request, LibraryEvent.class);
+        ResponseEntity<LibraryEvent> response =
+                restTemplate.exchange("/v1/library-event", HttpMethod.POST, request, LibraryEvent.class);
 
         // then
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(expectedRecord, responseBody);
     }
-
-
 }
